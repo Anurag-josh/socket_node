42b7b12c8550184671862bdebda789ead4b293bc
Anurag Joshi <anuragjoshi496@gmail.com>
2026-03-13 19:20:58 +0530

    Add project files

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*", // allow all origins
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+
+    // Listen for incoming posts
+    socket.on('send_post', (postData) => {
+        console.log('New post received:', postData);
+        // Broadcast the post to everyone EXCEPT the sender
+        socket.broadcast.emit('receive_post', postData);
+    });
+
+    // Listen for comments
+    socket.on('add_comment', (data) => {
+        console.log('New comment received:', data);
+        socket.broadcast.emit('receive_comment', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
+// Use 0.0.0.0 to accept connections from other devices on the same network
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Socket.IO Server is running on port ${PORT}`);
+    console.log(`Ensure your devices connect to http://<YOUR_LOCAL_IP>:${PORT}`);
+});
